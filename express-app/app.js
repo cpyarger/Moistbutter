c0d02723dc84cd411fff77d898d80eaac3d6e29d
Christopher P. Yarger <cpyarger@gmail.com>
2016-10-22 21:22:34 -0400

    Clean up on index.hbs

--- express-app/app.js
+++ express-app/app.js
@@ -1,20 +1,20 @@
 const express = require('express'),
       path = require('path'),
-      //favicon = require('serve-favicon'),
+      favicon = require('serve-favicon'),
       logger = require('morgan'),
       cookieParser = require('cookie-parser'),
       bodyParser = require('body-parser'),
       users = require('./router/routes/users'),
       app = express();
-
+//Set up full location for route path in filesystem
+app.set('routedir',path.join(__dirname, 'router/routes'));
 //view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('routedir',path.join(__dirname, 'router/routes'));
-//console.log(app.get('views'));
+
 app.set('view engine', 'hbs');
 app.set('env','development')
 //uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'fav.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,13 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/users', users);
+// Create router reference and pass "app" to the router, references Moistbutter/express-app/router/index.js
 var router = require('./router')(app);
-//catch 404 and forward to error handler
 
-//error handlers'development'
-
-//development error handler
-
-//production error handler
 
 module.exports = app;
